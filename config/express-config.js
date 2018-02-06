689ae81d45e0ad6b4730619cf38a97258694779c
Rafael Bernardo <rafamaxber@gmail.com>
2018-02-06 02:18:15 -0200

    add swagger in express-config

--- config/express-config.js
+++ config/express-config.js
@@ -3,16 +3,24 @@ const consign = require('consign');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const morgan = require('morgan');
-const Logger = require('../services/Logger');
 const dotenv = require('dotenv').config();
 const cors = require('cors')
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const Logger = require('../services/Logger');
 
 module.exports = () => {
   const app = express();
+  const routeSwaggerPath = '/api-docs';
 
   app.disable('x-powered-by');
 
   app.use(cors());
+
+  if (process.env.NODE_ENV !== 'test') {
+    const swaggerDocument = YAML.load('./swagger.yaml');
+    app.use(routeSwaggerPath, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  }
 
   app.use(morgan('common', {
     stream: {
